e7e7ac547c7e0a2fdf1f18c02384d3d1ad32687e
silverblade34 <mpacheco.tacay@gmail.com>
2024-11-09 08:13:32 -0500

    Change: Referencia al ARN dinamico

--- src/addNotification.js
+++ src/addNotification.js
@@ -1,18 +1,34 @@
 import middy from "@middy/core";
+import { v4 as uuidv4 } from "uuid";
 import jsonBodyParser from '@middy/http-json-body-parser'
 import AWS from "aws-sdk"
 
 const addNotification = async (event) => {
     try {
-        const notification = event.body;
+        const dynamodb = new AWS.DynamoDB.DocumentClient()
+        const id = uuidv4();
+        const notificationReceived = event.body;
+        const notification = {
+            ...notificationReceived,
+            id,
+            state: 0,
+            guid: notificationReceived.guid ? notificationReceived.guid : "",
+            linkVideo: notificationReceived.linkVideo ? notificationReceived.linkVideo : "",
+            linkImage: notificationReceived.linkImage ? notificationReceived.linkImage : "",
+            attentions: []
+        }
+        await dynamodb.put({
+            TableName: "ms4notificationTable",
+            Item: notification
+        }).promise()
         return {
             statusCode: 200,
-            body: JSON.stringify(notification),
+            body: JSON.stringify({ message: "Se ha registrado la notificacion correctamente", data: notification }),
         };
     } catch (error) {
         return {
             statusCode: 500,
-            body: JSON.stringify({ message: error.message }),
+            body: JSON.stringify({ message: error.message, data: null }),
         };
     }
 };
